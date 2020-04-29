fb3cc31bfb2f9cfff1876f20b5acdeb907d0c317
Pontus Broberg <pontus.broberg95@gmail.com>
2020-04-29 15:57:12 +0200

    Updates

---
+++ precache-manifest.9927c289a0335e5a91550b343e5fd69e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc579d3eb400dfbb28da903e92d6df4f",
+    "url": "/gh-pages-deploy-test/index.html"
+  },
+  {
+    "revision": "e19541b9661f98ac569b",
+    "url": "/gh-pages-deploy-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e7ebd54b5bec439229a6",
+    "url": "/gh-pages-deploy-test/static/js/2.3ae2d8b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-deploy-test/static/js/2.3ae2d8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e19541b9661f98ac569b",
+    "url": "/gh-pages-deploy-test/static/js/main.90c2f36e.chunk.js"
+  },
+  {
+    "revision": "8a6be5cab908543bc8ba",
+    "url": "/gh-pages-deploy-test/static/js/runtime-main.58723b24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
